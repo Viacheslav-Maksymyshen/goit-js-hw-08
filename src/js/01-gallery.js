89819dfaebd1afd41820ec35b300db2d3fedf76b
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-19 21:08:57 +0300

    upd files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const makeImagesItems = ({ preview, description, original }) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+const imagesItems = galleryItems.map(makeImagesItems).join('');
+gallery.insertAdjacentHTML('afterbegin', imagesItems);
+
+let modal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+modal.on('show.simplelightbox', function () {
+  console.log('ok');
+});
+
+modal.on('error.simplelightbox', function () {
+  console.log('error');
+});
 
 console.log(galleryItems);
