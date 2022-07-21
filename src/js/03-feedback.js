8b99d7f5f6b3581f26a0d5f79a86bb2f72669a06
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-21 21:31:49 +0300

    task -3 start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import { throttle } from 'lodash';
+const form = document.querySelector('feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
+const EMAIL_KEY = 'hw8-email-user';
+const MESSAGE_KEY = 'hw8-message-user';
+
+updateTextarea();
+
+form.addEventListener('submit', saveMessage);
+
+function saveMessage(evt) {
+  evt.preventDefault();
+
+  //   updateOutput();
+  form.reset();
+}
+
+function updateTextarea() {
+  input.textContent = localStorage.getItem(EMAIL_KEY) || '';
+  textarea.textContent = localStorage.getItem(MESSAGE_KEY) || '';
+  if (input.textContent || textarea.textContent) {
+    refs.textarea.value = savedMessage;
+  }
+}
