500f3e7a216d573974a745e547db0c4035a17b5a
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-07-21 16:30:45 +0300

    add vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,21 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
 });
+
+const onTimeupdate = function () {
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+  console.log('the time was updated to: ' + this.currentTime);
+  console.log('all times: ' + this.seeking);
+};
+player.on('timeupdate', onTimeupdate);
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
