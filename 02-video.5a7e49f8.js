c5197f303b4902836c6904fe4032a93a5eb62892
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-07-21 13:32:24 +0000

    Deploying to gh-pages from @ Viacheslav-Maksymyshen/goit-js-hw-08@500f3e7a216d573974a745e547db0c4035a17b5a 🚀

--- 02-video.5a7e49f8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.5a7e49f8.js.map
--- 02-video.b50b3e81.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.b50b3e81.js.map
