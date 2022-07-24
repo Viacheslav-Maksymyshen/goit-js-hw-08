adca712aa1adacadc064507fb9e110c3b0c2e82f
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-24 12:43:34 +0300

    Task - 3 added logic

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,29 +10,44 @@ updateDataForm();
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(saveDataForm, 500));
 
-const formData = {};
+// const formData = {};
 
 function onFormSubmit(evtent) {
   evtent.preventDefault();
+  if (textarea.value === '' || input.value === '') {
+    alert('Some field is empty');
+    return;
+  }
   evtent.currentTarget.reset();
   console.log(JSON.parse(localStorage.getItem(MESSAGE_KEY)));
   localStorage.removeItem(MESSAGE_KEY);
+  alert('Feedback has been sent');
 }
 
 function saveDataForm(evtent) {
-  formData[evtent.target.name] = evtent.target.value;
-  localStorage.setItem(MESSAGE_KEY, JSON.stringify(formData));
+  // formData[evtent.target.name] = evtent.target.value;
+  // localStorage.setItem(MESSAGE_KEY, JSON.stringify(formData));
+
+  const emailValue = input.value;
+  const messageValue = textarea.value;
+
+  localStorage.setItem(
+    MESSAGE_KEY,
+    JSON.stringify({ email: emailValue, message: messageValue })
+  );
 }
 
 function updateDataForm() {
   const dataContent = localStorage.getItem(MESSAGE_KEY) || '';
-  if (dataContent) {
-    try {
-      const convertingDataContent = JSON.parse(dataContent);
+  try {
+    const convertingDataContent = JSON.parse(dataContent);
+    if (convertingDataContent.message) {
       textarea.value = convertingDataContent.message;
-      input.value = convertingDataContent.email;
-    } catch (err) {
-      console.log('error');
     }
+    if (convertingDataContent.email) {
+      input.value = convertingDataContent.email;
+    }
+  } catch (err) {
+    console.log('error');
   }
 }
